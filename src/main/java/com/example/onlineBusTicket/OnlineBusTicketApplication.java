12ea8324171f0e6199f89c08251708d1811e0750
Jawher Ben Amor <benamorjawher.isbs@gmail.com>
2025-12-30 09:37:43 +0100

    chore: init spring boot project (java21, web, jpa, h2)

---
+++ src/main/java/com/example/onlineBusTicket/OnlineBusTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineBusTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBusTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBusTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineBusTicket/OnlineBusTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineBusTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBusTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
